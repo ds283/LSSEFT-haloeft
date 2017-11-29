Fix calculation of average error in each P_ell
- this is performed as part of the analysis pipeline, but previously contained a bug which meant the results were meaningless

analyse.py
@@ -71,8 +71,8 @@ class EFT_tools(heft.HaloEFT_core):
                 for region in self.data_regions:
 
                     mask = self.conv_to_means_mask
-                    means = self.data_all_means[region]
-                    cov = self.data_all_covs[region]
+                    means = self.data_fit_means[region]
+                    cov = self.data_fit_covs[region]
                     conv = self.data_convs[region]
 
                     Pconv = np.dot(conv, P)
@@ -116,10 +116,12 @@ class EFT_tools(heft.HaloEFT_core):
             writer = csv.DictWriter(f, labels)
             writer.writeheader()
 
+            # loop over each k sample point to be included in the analysis
             for i in xrange(len(self.WiggleZ_mean_ks)):
 
                 row = {'k': self.WiggleZ_mean_ks[i], 'P0': P0[i], 'P2': P2[i], 'P4': P4[i]}
 
+                # initialize accumulators used to sum over all regions
                 P0_theory_tot = 0
                 P2_theory_tot = 0
                 P4_theory_tot = 0
@@ -132,6 +134,7 @@ class EFT_tools(heft.HaloEFT_core):
                 P2_Delta_tot = 0
                 P4_Delta_tot = 0
 
+                # loop over regions
                 for r in self.data_regions:
 
                     means = self.data_raw_means[r]
@@ -139,25 +142,37 @@ class EFT_tools(heft.HaloEFT_core):
 
                     Pconv = np.dot(conv, P)
 
-                    row.update({r+'_P0_theory': Pconv[i], r+'_P0_WizCOLA': means[i]})
-                    row.update({r+'_P2_theory': Pconv[1 * self.nbinc + i], r + '_P2_WizCOLA': means[1 * self.nbin + i]})
-                    row.update({r+'_P4_theory': Pconv[2 * self.nbinc + i], r + '_P4_WizCOLA': means[2 * self.nbin + i]})
+                    P0_theory = Pconv[0 * self.nbinc + i]
+                    P2_theory = Pconv[1 * self.nbinc + i]
+                    P4_theory = Pconv[2 * self.nbinc + i]
 
-                    row.update({r+'_P0_Delta': row[r+'_P0_theory'] - row[r+'_P0_WizCOLA']})
-                    row.update({r+'_P2_Delta': row[r+'_P2_theory'] - row[r+'_P2_WizCOLA']})
-                    row.update({r+'_P4_Delta': row[r+'_P4_theory'] - row[r+'_P4_WizCOLA']})
+                    P0_data = means[0 * self.nbin + i]
+                    P2_data = means[1 * self.nbin + i]
+                    P4_data = means[2 * self.nbin + i]
 
-                    P0_theory_tot += row[r+'_P0_theory']
-                    P2_theory_tot += row[r+'_P2_theory']
-                    P4_theory_tot += row[r+'_P4_theory']
+                    row.update({r+'_P0_theory': P0_theory, r + '_P0_WizCOLA': P0_data})
+                    row.update({r+'_P2_theory': P2_theory, r + '_P2_WizCOLA': P2_data})
+                    row.update({r+'_P4_theory': P4_theory, r + '_P4_WizCOLA': P4_data})
 
-                    P0_WizCOLA_tot += row[r+'_P0_WizCOLA']
-                    P2_WizCOLA_tot += row[r+'_P2_WizCOLA']
-                    P4_WizCOLA_tot += row[r+'_P4_WizCOLA']
+                    P0_delta = P0_data - P0_theory
+                    P2_delta = P2_data - P2_theory
+                    P4_delta = P4_data - P4_theory
 
-                    P0_Delta_tot += P0_theory_tot - P0_WizCOLA_tot
-                    P2_Delta_tot += P2_theory_tot - P2_WizCOLA_tot
-                    P4_Delta_tot += P4_theory_tot - P4_WizCOLA_tot
+                    row.update({r+'_P0_Delta': P0_delta})
+                    row.update({r+'_P2_Delta': P2_delta})
+                    row.update({r+'_P4_Delta': P4_delta})
+
+                    P0_theory_tot += P0_theory
+                    P2_theory_tot += P2_theory
+                    P4_theory_tot += P4_theory
+
+                    P0_WizCOLA_tot += P0_data
+                    P2_WizCOLA_tot += P2_data
+                    P4_WizCOLA_tot += P4_data
+
+                    P0_Delta_tot += P0_delta
+                    P2_Delta_tot += P2_delta
+                    P4_Delta_tot += P4_delta
 
                 regions = len(self.data_regions)
 

haloeft.py
@@ -97,9 +97,9 @@ class HaloEFT_core(object):
         h22_cov = my_config["HaloEFT", "h22_matrix"]
 
         self.data_raw_means = {}
-        self.data_all_means = {}
+        self.data_fit_means = {}
         self.data_ren_means = {}
-        self.data_all_covs = {}
+        self.data_fit_covs = {}
         self.data_ren_covs = {}
         self.data_convs = {}
         self.data_regions = ['1h', '3h', '8h', '11h', '15h', '22h']
@@ -145,7 +145,7 @@ class HaloEFT_core(object):
         this_P4 = np.asarray(mean_realization['P4'])
 
         self.data_raw_means[tag] = np.concatenate((this_P0, this_P2, this_P4))
-        self.data_all_means[tag] = np.concatenate((this_P0, this_P2, this_P4))[self.mean_fit_mask]
+        self.data_fit_means[tag] = np.concatenate((this_P0, this_P2, this_P4))[self.mean_fit_mask]
         self.data_ren_means[tag] = np.concatenate((this_P0, this_P2, this_P4))[self.__mean_ren_mask]
 
         CMat = np.empty((3*nbin, 3*nbin))
@@ -164,9 +164,9 @@ class HaloEFT_core(object):
         w, p = np.linalg.eig(CMat_all)
         if not np.all(w > 0):
             print 'using pseudo-inverse covariance matrix for "all" group in region {tag}'.format(tag=tag)
-            self.data_all_covs[tag] = np.linalg.pinv(CMat_all)
+            self.data_fit_covs[tag] = np.linalg.pinv(CMat_all)
         else:
-            self.data_all_covs[tag] = np.linalg.inv(CMat_all)
+            self.data_fit_covs[tag] = np.linalg.inv(CMat_all)
 
         CMat_ren = (CMat[self.__mean_ren_mask, :])[:, self.__mean_ren_mask]
 
@@ -407,11 +407,14 @@ class HaloEFT_core(object):
     def build_theory_P_ell(self, coeffs):
 
         # construct P_ell, including mixing counterterms and stochastic counterterms
+
+        # this implementation is a bit cryptic but has been tuned for speed -- this is the rate-limiting
+        # step in an MCMC analysis
         zip = [ coeffs[key] * data for key, data in self.theory_payload.iteritems() ]
 
         P = zip[0].copy()
         for a in zip[1:]:
-            P += a
+            P += a              # in-place addition is fastest
 
         return P[0], P[1], P[2]
 
@@ -420,8 +423,8 @@ class HaloEFT_core(object):
 
         if type is 'all':
             mask = self.conv_fit_mask
-            means = self.data_all_means[region]
-            cov = self.data_all_covs[region]
+            means = self.data_fit_means[region]
+            cov = self.data_fit_covs[region]
         else:
             mask = self.__conv_ren_mask
             means = self.data_ren_means[region]