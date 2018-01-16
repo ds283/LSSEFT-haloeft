ab50db53fbdd7c5fbf62e5f086bdd127c1852251
ds283 <D.Seery@sussex.ac.uk>
2018-01-16 12:21:44 +0000

    Fix error in Apollo deployment path

--- tools/deploy.py
+++ tools/deploy.py
@@ -3,7 +3,7 @@ import version as release
 
 # DEPLOYMENT PARAMETERS
 
-release_name = "LSSEFT_{tag}".format(tag=release.version)
+release_name = "LSSEFT-haloeft_{tag}".format(tag=release.version)
 deploy_root = os.path.join("/", "home", "d", "ds", "ds283", "LSSEFT-haloeft", release_name)
 cosmosis_executable = "cosmosis"
 MPI_processes = 64
