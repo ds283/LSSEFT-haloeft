dff36bb247a30a78cf6c7d663e54aad90932f9b6
ds283 <D.Seery@sussex.ac.uk>
2018-01-25 19:47:42 +0000

    Allow specification of user local site-packages directory on PYTHONPATH
    - this is required if Python doesn’t add the user site-packages automatically, which can happen under some circumstances to avoid injection attacks. This seems to happen when Python is running under CosmoSIS

--- tools/deploy.py
+++ tools/deploy.py
@@ -1,8 +1,11 @@
 import os
+import version as release
 
 # DEPLOYMENT PARAMETERS
 
+release_name = "LSSEFT-haloeft_{tag}".format(tag=release.version)
 deploy_root = os.path.join("LSSEFT-haloeft")
+local_site = None
 cosmosis_executable = "./bin/cosmosis"
 MPI_processes = 8
 email = "D.Seery@sussex.ac.uk"
